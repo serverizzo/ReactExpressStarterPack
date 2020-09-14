f0cc1742249a528e681c3e100bcf8d3326f0ce6c
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-09-14 16:25:25 -0400

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
+var path = require('path');
 
 app.get('/api/customers', (req, res) => {
   const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    { id: 1, firstName: 'John', lastName: 'Doe' },
+    { id: 2, firstName: 'Brad', lastName: 'Traversy' },
+    { id: 3, firstName: 'Mary', lastName: 'Swanson' },
   ];
 
   res.json(customers);
@@ -15,3 +16,7 @@ app.get('/api/customers', (req, res) => {
 const port = 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
